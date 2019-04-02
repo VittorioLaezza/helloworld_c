ad3bc5b981d0cbbfefee9d1ac7fa40c165199cd8
Vittorio <vitty157@gmail.com>
2019-04-02 21:58:58 +0200

    file

---
+++ maggiore2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+int a,b,M;
+printf("dammi due numeri per il confronto\n");
+scanf("%d",&a);
+scanf("%d",&b);
+M=a;
+if(b>M) M=b;
+
+printf("il massimo è: %d",M);
+return 0;
+
+
+
+
+
+
+
+}
